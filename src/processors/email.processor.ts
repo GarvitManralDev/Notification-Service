efc5cf67653540ff9419bae423e1004ff9706da2
G-a-r-r-y <manralgarv@gmail.com>
2025-06-19 15:06:08 +0530

    Initial Commit

---
+++ src/processors/email.processor.ts
@@ -0,0 +1,31 @@
+import { Job, Worker } from "bullmq";
+import { MAILER_QUEUE } from "../queues/mailer.queue";
+import { getRedisConnObject } from "../config/redis.config";
+import { NotificationDto } from "../dto/notification.dto";
+import { MAILER_PAYLOAD } from "../producers/email.producer";
+
+export const setupMailerWorker = () => {
+  const emailProcessor = new Worker<NotificationDto>(
+    MAILER_QUEUE, //Name of queue
+    async (job: Job) => {
+      if (job.name !== MAILER_PAYLOAD) {
+        throw new Error("Invalid job name");
+      }
+
+      //call the service layer from here
+      const payload = job.data;
+      console.log(`Processing email for:${JSON.stringify(payload)}`);
+    }, //Process function
+    {
+      connection: getRedisConnObject(),
+    }
+  );
+
+  emailProcessor.on("failed", () => {
+    console.error("Email processing failed");
+  });
+
+  emailProcessor.on("completed", () => {
+    console.log("Email processing completed successfully");
+  });
+};
